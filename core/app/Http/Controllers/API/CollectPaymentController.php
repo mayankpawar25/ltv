4abeb9bc98c86a53b277e87899cbfdfa4a50aed1
Sawan Shakya <Developer@webcraft.co.in>
2019-11-28 19:15:40 +0530

    Update

--- core/app/Http/Controllers/API/CollectPaymentController.php
+++ core/app/Http/Controllers/API/CollectPaymentController.php
@@ -172,7 +172,7 @@ class CollectPaymentController extends Controller
                 return response()->json($data, $status);
             }
 
-            $threads = PaymentCollectionDescription::where('payment_collection_id',$paymentcollection->id)->get();
+            $threads = PaymentCollectionDescription::where('payment_collection_id',$collection_payment_id)->get();
 
             if(auth()->user()->id == $check_collection_id->staff_user_id && count($threads) % \Config::get('constants.THREAD_COUNT')-1 == 0){
                 if($request->payment_type == ''){
