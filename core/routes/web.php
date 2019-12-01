0ff7eaff1c5d10a0000496206e4c32d0f0bd6dad
Rahul Shardiya <rahul.shardiya@webcraft.co.in>
2019-12-01 17:35:41 +0530

    update files

--- core/routes/web.php
+++ core/routes/web.php
@@ -28,7 +28,7 @@ Route::get('/shop', function () {
     return redirect('admin');
 });*/
 Route::post('/paginate', 'Admin\AreaController@paginate')->name('datatables_area');
-
+Route::get('/import/download/sample', 'Admin\AreaController@download_sample_area_import_file')->name('download_sample_area_import_file');
 Route::get('qrcode', 'QrcodeController@generateQrCode')->name('generateqrcode');
 
 //Payment IPN
@@ -1516,7 +1516,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth:admin','set_user_permi
 	      ]
 	]);
 
-	Route::get('/import/download/sample', 'Admin\AreaController@download_sample_area_import_file')->name('download_sample_area_import_file');
+	
     Route::get('/delete/{id}', 'Admin\AreaController@destroy')->name('area.delete');
 
     Route::get('/import_page', 'Admin\AreaController@import_page')->name('area.import_page');
