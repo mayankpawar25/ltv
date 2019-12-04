8921ae95500f7ddc6b854793ebf3b0847243aa49
Sawan Shakya <Developer@webcraft.co.in>
2019-12-04 13:04:20 +0530

    UPdate

--- core/app/Http/Controllers/API/UserController.php
+++ core/app/Http/Controllers/API/UserController.php
@@ -423,13 +423,15 @@ class UserController extends Controller
         foreach($products as $p_key => $p_value){
           $attr = [];
           $i = 0;
-          $attributes = json_decode($p_value->attributes);
-          foreach ($attributes as $key => $attribute) {
-              $attr[$i]['name'] = $key;
-              $attr[$i]['options'] = $attribute[0];
-              $i++;
+          if($p_value->attributes!=''){
+            $attributes = json_decode($p_value->attributes);
+            foreach ($attributes as $key => $attribute) {
+                $attr[$i]['name'] = $key;
+                $attr[$i]['options'] = $attribute[0];
+                $i++;
+            }
+            $p_value->attributes = $attr;
           }
-          $p_value->attributes = $attr;
         }
 
 
