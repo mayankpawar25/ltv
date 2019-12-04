53ee99df4c87b3d4d152307e78e6dd30b2d19fbf
Sawan Shakya <Developer@webcraft.co.in>
2019-12-04 13:21:05 +0530

    Order History

--- core/app/Http/Controllers/API/UserController.php
+++ core/app/Http/Controllers/API/UserController.php
@@ -423,8 +423,8 @@ class UserController extends Controller
         foreach($products as $p_key => $p_value){
           $attr = [];
           $i = 0;
-          $attributes = json_decode($p_value->attributes);
-          if($attributes!='[]' || $attributes!='' || $attributes!='""'){
+          if($p_value->attributes!='[]' || $p_value->attributes!='' || $p_value->attributes!='""'){
+            $attributes = json_decode($p_value->attributes);
             foreach ($attributes as $key => $attribute) {
                 $attr[$i]['name'] = $key;
                 $attr[$i]['options'] = (isset($attribute[0]))?$attribute[0]:'';
