d7b6d9d077476fb3d6ad960c2745626a90303f68
Rahul Shardiya <rahul.shardiya@webcraft.co.in>
2019-12-05 18:46:06 +0530

    update customer

--- core/resources/views/admin/crm/customer/index.blade.php
+++ core/resources/views/admin/crm/customer/index.blade.php
@@ -1,6 +1,30 @@
 @extends('admin.layout.master')
 @section('title', __('form.customers'))
 @section('content')
+<style type="text/css" media="screen">
+.dataTables_length, .dt-buttons {
+    float: left;
+    width: 100%;
+}
+
+.dataTables_wrapper .dt-buttons {
+    float: left;
+    text-align: center;
+    width: auto;
+}
+div.dataTables_wrapper div.dataTables_filter {
+    text-align: right;
+    width: auto;
+}
+div#data_filter {
+    display: none;
+}
+#data tr td:last-child {
+  text-align: right;
+}
+
+</style>
+
 <div class="app-content">
 
 <div class="main-content">
@@ -103,25 +127,26 @@ table tr td:last-child{
 
 
             dataTable = $('#data').DataTable({
-                dom: 'Bfrtip',
-                buttons: [
-
-                    {
-                        init: function(api, node, config) {
-                            $(node).removeClass('btn-secondary')
-                        },
-                        className: "btn-light btn-sm",
-                        extend: 'collection',
-                        text: 'Export',
-                        buttons: [
-                            'copy',
-                            'excel',
-                            'csv',
-                            'pdf',
-                            'print'
-                        ]
-                    }
-                ],
+            dom: 'lfBfrtip',
+            buttons: [
+                {
+                  extend: 'copyHtml5',
+                  exportOptions: {
+                      columns: ':visible'
+                  }
+                },{
+                  extend: 'excelHtml5',
+                  exportOptions: {
+                    columns: ':visible'
+                  }
+                },{
+                  extend: 'print',
+                  exportOptions: {
+                    columns: ':visible'
+                  }
+                },
+                'colvis'
+              ],
 
                 "language": {
                     "lengthMenu": '_MENU_ ',
