7910ea5d291be7b7dd37327bec4896662bbc7129
AjAy Lowanshi <ajay@webcraft.co.in>
2019-12-10 16:40:08 +0530

    general settings update

--- core/resources/views/admin/GeneralSettings.blade.php
+++ core/resources/views/admin/GeneralSettings.blade.php
@@ -1,160 +1,154 @@
 @extends('admin.layout.master')
 
 @section('content')
-  <main class="app-content">
-   <div class="main-content"> 
-   <h5>General Settings</h5>
-   <hr />  
-     <div class="row">
-     
-        <div class="col-md-12">
-           <div class="">
-              <div class="">
-                 <form role="form" method="POST" action="{{route('admin.UpdateGenSetting')}}">
-                    {{csrf_field()}}
-                    <div class="row">
-                       <div class="col-md-3">
-                          <label>Website Title</label>
-                          <div class="">
-                             <input name="websiteTitle" type="text" class="form-control " value="{{$gs->website_title}}">
-                            <!-- <div class="input-group-append"><span class="input-group-text">
-                                <i class="fa fa-file-text-o"></i>
-                                </span>
-                             </div>-->
-                          </div>
-                          @if ($errors->has('websiteTitle'))
-                            <span style="color:red;">{{$errors->first('websiteTitle')}}</span>
-                          @endif
-                          <span class="text-danger"></span>
-                       </div>
-
-                       <div class="col-md-3">
-                          <label>Site Base Color (Without #)</label>
-                          <div class="input-group">
-                             <input style="background-color:#{{$gs->base_color_code}}" type="text" class="form-control " value="{{$gs->base_color_code}}" name="baseColorCode">
-                             <div class="input-group-append"><span class="input-group-text">
-                                <i class="fa fa-paint-brush"></i>
-                                </span>
-                             </div>
-                          </div>
-                          @if ($errors->has('baseColorCode'))
-                            <span style="color:red;">{{$errors->first('baseColorCode')}}</span>
-                          @endif
-                       </div>
-                       <div class="col-md-2">
-                          <label>Base Currency Text</label>
-                          <div class="">
-                             <input type="text" class="form-control " value="{{$gs->base_curr_text}}" name="baseCurrencyText">
-                             <!--<div class="input-group-append"><span class="input-group-text">
-                                <i class="fa fa fa-money"></i>
-                                </span>
-                             </div>-->
-                          </div>
-                          @if ($errors->has('baseCurrencyText'))
-                            <span style="color:red;">{{$errors->first('baseCurrencyText')}}</span>
-                          @endif
-                       </div>
-                       <div class="col-md-2">
-                          <label>Base Currency Symbol</label>
-                          <div class="">
-                             <input type="text" class="form-control " value="{{$gs->base_curr_symbol}}" name="baseCurrencySymbol">
-                            <!-- <div class="input-group-append"><span class="input-group-text">
-                                <i class="fa fa fa-money"></i>
-                                </span>
-                             </div>-->
-                          </div>
-                          @if ($errors->has('baseCurrencySymbol'))
-                            <span style="color:red;">{{$errors->first('baseCurrencySymbol')}}</span>
-                          @endif
-                       </div>
-                       <div class="col-md-2">
-                          <label>Main Hub Location</label>
-                          <div class="">
-                             <input type="text" class="form-control " value="{{$gs->main_city}}" name="main_city">
-                             <!--<div class="input-group-append"><span class="input-group-text">
-                                <i class="fa fa fa-money"></i>
-                                </span>
-                             </div>-->
-                          </div>
-                          @if ($errors->has('baseCurrencySymbol'))
-                            <span style="color:red;">{{$errors->first('baseCurrencySymbol')}}</span>
-                          @endif
-                       </div>
-                    </div>
-                    <br>
-                    <div class="row">
-
-                       <div class="col-md-3">
-                          <div class="check-round">
-                          <label>Email Verification</label>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="emailVerification" {{$gs->email_verification == 0 ? 'checked' : ''}}>
-                             </div>
-                       </div>
-                       <div class="col">
-                          <label>SMS Verification</label>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="smsVerification" {{$gs->sms_verification == 0 ? 'checked' : ''}}>
-                       </div>
-                       <div class="col">
-                          <label>Email Notification</label>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="emailNotification" {{$gs->email_notification == 1 ? 'checked' : ''}}>
-                       </div>
-
-                       <div class="col">
-                          <label>SMS Notification</label>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="smsNotification" {{$gs->sms_notification == 1 ? 'checked' : ''}}>
-                       </div>
-                       <div class="col">
-                          <label>REGISTRATION</label>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="registration" {{$gs->registration == 1 ? 'checked' : ''}}>
-                       </div>
-                    </div>
-                    <br>
-
-                    <div class="row">
-                       <div class="col-md-3">
-                          <h6>Facebook Login Status</h6>
-                          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
-                             data-width="100%" type="checkbox"
-                             name="status" {{$provider->status == 1 ? 'checked' : ''}}>
-                       </div>
-                       <div class="col">
-                         <h6>Facebook APP ID</h6>
-                         <input class="form-control " name="app_id" value="{{$provider->client_id}}" type="text">
-                         @if ($errors->has('app_id'))
-                           <p class="text-danger">{{$errors->first('app_id')}}</p>
-                         @endif
-                       </div>
-                       <div class="col">
-                          <h6>Facebook APP Secret</h6>
-                          <input class="form-control " name="app_secret" value="{{$provider->client_secret}}" type="text">
-                          @if ($errors->has('app_secret'))
-                            <p class="text-danger">{{$errors->first('app_secret')}}</p>
-                          @endif
-                       </div>
-                    </div>
-                    <br>
-                    <div class="row">
-                       <hr>
-                       <div class="col-md-12 text-right">
-                       <hr />
-                          <button type="submit" class="btn btn-success ">UPDATE</button>
-                       </div>
-                    </div>
-                 </form>
+    <main class="app-content">
+      <div class="main-content">
+        <h5>General Settings</h5>
+        <hr />
+          <div class="">
+            <form role="form" method="POST" action="{{route('admin.UpdateGenSetting')}}">
+              <div class="row">
+              {{csrf_field()}}
+              <div class="col-md-6">
+                <div class="row">
+                  <div class="col-md-6">
+                    <label>Website Title</label>
+                    <div class="">
+                      <input name="websiteTitle" type="text" class="form-control " value="{{$gs->website_title}}">
+                      <!-- <div class="input-group-append"><span class="input-group-text">
+                        <i class="fa fa-file-text-o"></i>
+                      </span>
+                    </div>-->
+                  </div>
+                  @if ($errors->has('websiteTitle'))
+                  <span style="color:red;">{{$errors->first('websiteTitle')}}</span>
+                  @endif
+                  <span class="text-danger"></span>
+                </div>
+                <div class="col-md-6">
+                  <label>Site Base Color (Without #)</label>
+                  <div class="input-group">
+                    <input style="background-color:#{{$gs->base_color_code}}" type="text" class="form-control " value="{{$gs->base_color_code}}" name="baseColorCode">
+                    <div class="input-group-append"><span class="input-group-text">
+                      <i class="fa fa-paint-brush"></i>
+                    </span>
+                  </div>
+                </div>
+                @if ($errors->has('baseColorCode'))
+                <span style="color:red;">{{$errors->first('baseColorCode')}}</span>
+                @endif
               </div>
-           </div>
+              <div class="col-md-6">
+                <label>Base Currency Text</label>
+                <div class="">
+                  <input type="text" class="form-control " value="{{$gs->base_curr_text}}" name="baseCurrencyText">
+                  <!--<div class="input-group-append"><span class="input-group-text">
+                    <i class="fa fa fa-money"></i>
+                  </span>
+                </div>-->
+              </div>
+              @if ($errors->has('baseCurrencyText'))
+              <span style="color:red;">{{$errors->first('baseCurrencyText')}}</span>
+              @endif
+            </div>
+            <div class="col-md-6">
+              <label>Base Currency Symbol</label>
+              <div class="">
+                <input type="text" class="form-control " value="{{$gs->base_curr_symbol}}" name="baseCurrencySymbol">
+                <!-- <div class="input-group-append"><span class="input-group-text">
+                  <i class="fa fa fa-money"></i>
+                </span>
+              </div>-->
+            </div>
+            @if ($errors->has('baseCurrencySymbol'))
+            <span style="color:red;">{{$errors->first('baseCurrencySymbol')}}</span>
+            @endif
+          </div>
+          <div class="col-md-12">
+            <label>Main Hub Location</label>
+            <div class="">
+              <input type="text" class="form-control " value="{{$gs->main_city}}" name="main_city">
+              <!--<div class="input-group-append"><span class="input-group-text">
+                <i class="fa fa fa-money"></i>
+              </span>
+            </div>-->
+          </div>
+          @if ($errors->has('baseCurrencySymbol'))
+          <span style="color:red;">{{$errors->first('baseCurrencySymbol')}}</span>
+          @endif
         </div>
-     </div>
-     </div>
-  </main>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="check-round">
+            <label>Email Verification</label>
+            <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+            data-width="100%" type="checkbox"
+            name="emailVerification" {{$gs->email_verification == 0 ? 'checked' : ''}}>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <label>SMS Verification</label>
+          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+          data-width="100%" type="checkbox"
+          name="smsVerification" {{$gs->sms_verification == 0 ? 'checked' : ''}}>
+        </div>
+        <div class="col-md-6">
+          <label>Email Notification</label>
+          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+          data-width="100%" type="checkbox"
+          name="emailNotification" {{$gs->email_notification == 1 ? 'checked' : ''}}>
+        </div>
+        <div class="col-md-6">
+          <label>SMS Notification</label>
+          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+          data-width="100%" type="checkbox"
+          name="smsNotification" {{$gs->sms_notification == 1 ? 'checked' : ''}}>
+        </div>
+        <div class="col-md-6">
+          <label>Registration</label>
+          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+          data-width="100%" type="checkbox"
+          name="registration" {{$gs->registration == 1 ? 'checked' : ''}}>
+        </div>
+        <div class="col-md-6">
+          <label>Facebook Login Status</label>
+          <input data-toggle="toggle" data-onstyle="success" data-offstyle="danger"
+          data-width="100%" type="checkbox"
+          name="status" {{$provider->status == 1 ? 'checked' : ''}}>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12">
+      <div class="row">
+        
+        <div class="col">
+          <h6>Facebook APP ID</h6>
+          <input class="form-control " name="app_id" value="{{$provider->client_id}}" type="text">
+          @if ($errors->has('app_id'))
+          <p class="text-danger">{{$errors->first('app_id')}}</p>
+          @endif
+        </div>
+        <div class="col">
+          <h6>Facebook APP Secret</h6>
+          <input class="form-control " name="app_secret" value="{{$provider->client_secret}}" type="text">
+          @if ($errors->has('app_secret'))
+          <p class="text-danger">{{$errors->first('app_secret')}}</p>
+          @endif
+        </div>
+      </div>
+    </div>
+    <br>
+      <hr>
+      <div class="col-md-12 text-right">
+        <hr />
+        <button type="submit" class="btn btn-success ">UPDATE</button>
+      </div>
+  </div>
+    </form>
+    </div>
+    </div>
+    </main>
 @endsection
--- core/resources/views/admin/subscribers/index.blade.php
+++ core/resources/views/admin/subscribers/index.blade.php
@@ -18,70 +18,73 @@
 @endpush
 
 @section('content')
-  <main class="app-content">
-     <div class="app-title">
+    <main class="app-content">
+      {{-- <div class="app-title">
         <div>
-           <h1><i class="fa fa-dashboard"></i> Send News Letter</h1>
+          <h1><i class="fa fa-dashboard"></i> Send News Letter</h1>
         </div>
         <ul class="app-breadcrumb breadcrumb">
-           <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-           <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+          <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
         </ul>
-     </div>
-     <div class="row">
+      </div> --}}
+      <div class="row">
+
         <div class="col-md-12">
-
-          <div class="tile">
-            <div class="row">
-
-              <div class="col-md-12">
-                <form role="form" method="POST" action="{{route('admin.mailtosubsc')}}" enctype="multipart/form-data">
-                   {{ csrf_field() }}
-                   <div class="form-body">
-                      <div class="form-group">
-                         <label>Subject</label>
-                         <input type="text" name="subject" class="form-control input-lg" value="">
-                         @if ($errors->has('subject'))
-                           <p style="color:red;">{{$errors->first('subject')}}</p>
-                         @endif
-                      </div>
-                      <div class="form-group">
-                         <label>Email Message</label>
-                         <textarea id="message" class="form-control" name="message" rows="5">
-                         </textarea>
-                         @if ($errors->has('message'))
-                           <p style="color:red;">{{$errors->first('message')}}</p>
-                         @endif
-                      </div>
-                   </div>
-                   <div class="form-actions">
-                      <button type="submit" class="submit-btn btn btn-primary btn-lg btn-block login-button">Broadcast Email</button>
-                   </div>
-                </form>
-              </div>
-
-            </div>
-            <br>
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="card">
-                  <div class="card-header bg-primary">
-                    <h3 class="text-white"><i class="fa fa-list"></i> ALL SUBSCRIBERS</h3>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="main-content">
+                <h5>All Subscribers</h5><hr>
+                <div class="">
+                  <div class="">
                   </div>
-                  <div class="card-body">
+                  <div class="subscriber-body">
+                    <ul class="pl-3">
                     @foreach ($subscribers as $subscriber)
-                      {{$subscriber->email}}
-                      @if (!$loop->last)
-                        , &nbsp;
-                      @endif
+                      <li>
+                        {{$subscriber->email}}
+                      </li>
                     @endforeach
+                    </ul>
+                    {{--  @if (!$loop->last)
+                    , &nbsp;
+                    @endif --}}
                   </div>
                 </div>
               </div>
             </div>
+
+            <div class="col-md-8">
+              <div class="main-content">
+                <h5>Send News Letter</h5><hr>
+                <form role="form" method="POST" action="{{route('admin.mailtosubsc')}}" enctype="multipart/form-data">
+                  {{ csrf_field() }}
+                  <div class="form-body">
+                    <div class="form-group">
+                      <label>Subject</label>
+                      <input type="text" name="subject" class="form-control input-lg" value="">
+                      @if ($errors->has('subject'))
+                      <p style="color:red;">{{$errors->first('subject')}}</p>
+                      @endif
+                    </div>
+                    <div class="form-group">
+                      <label>Email Message</label>
+                      <textarea id="message" class="form-control" name="message" rows="5" style="min-height: 200px">
+                      </textarea>
+                      @if ($errors->has('message'))
+                      <p style="color:red;">{{$errors->first('message')}}</p>
+                      @endif
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="form-actions text-right">
+                    <button type="submit" class="btn btn-success login-button">Broadcast Email</button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-     </div>
-  </main>
+      </div>
+    </main>
 @endsection
