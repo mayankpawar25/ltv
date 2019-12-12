8bc9c0520764042d2c9515632ec921e407f654b1
Rahul Shardiya <rahul.shardiya@webcraft.co.in>
2019-12-12 11:38:16 +0530

    update task status

--- core/app/Task.php
+++ core/app/Task.php
@@ -6,17 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['name', 'description', 'task_date'];
+    protected $fillable = ['name', 'description', 'task_date','task_status_id'];
 
     public function zipcode() {
       return $this->hasMany('App\Zipcode');
     }
 
-    public function taskstatus() {
-      return $this->belongsTo('App\Product');
+    function taskstatus() {
+      return $this->belongsTo('App\TaskStatus','task_status_id','id');
     }
 
     public function salesman() {
       return $this->belongsTo('App\Models\StaffUser');
     }
+
+
 }
